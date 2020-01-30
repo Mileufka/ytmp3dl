d067d1e3e026ee5971f92399e7d6c25e73104da8
Mileufka <mileufka@gmail.com>
2020-01-30 12:00:48 +0100

    Retrieved tagger.py from former version of the script.

---
+++ tagger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#tagger.py
+
+import sys
+from mutagen.easyid3 import EasyID3
+
+metatag = EasyID3(sys.argv[1])
+metatag['title'] = sys.argv[2]
+
+#print("title : ", metatag['title'])
+
+if len(sys.argv) >= 4:
+    metatag['artist'] = sys.argv[3]
+#    if len(sys.argv) >= 5:
+#        metatag['date'] = int(sys.argv[4])
+
+metatag.save()
+
+#print('Number of arguments:', len(sys.argv), 'arguments.')
+#print('Argument list:', str(sys.argv))
+
+#print(arg1)
+#print(arg2)
+#print(arg3)
